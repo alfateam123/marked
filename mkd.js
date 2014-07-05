2823f26c615a0d100c8b703e2f4ed7a0fda533a2
alfateam123 <alfateam123@gmail.com>
2014-07-05 12:08:52 +0200

    Add homepage (index.html, mkd.js)
    This commit lets you test this library in the browser.
    based on the md2bbc (a failed test) index.html page

---
+++ mkd.js
@@ -0,0 +1,57 @@
+
+/*
+var conv_opts = {
+	multiline_quoting: false,
+	check_quotes_into_lists: false,
+	recognize_bbcode: false,
+	enable_autolinking: false
+};
+
+var togglemultiline = function(){
+	conv_opts.multiline_quoting = !conv_opts.multiline_quoting;
+	console.log("(un)checked, now ", conv_opts);
+}
+
+var togglequotesintolists = function(){
+	conv_opts.check_quotes_into_lists = !conv_opts.check_quotes_into_lists;
+	console.log("(un)checked, now ", conv_opts);
+}
+
+var togglebbcode = function(){
+	conv_opts.recognize_bbcode = !conv_opts.recognize_bbcode;
+	console.log("(un)checked, now ", conv_opts);
+}
+
+var toggleautolinking = function(){
+	conv_opts.enable_autolinking = !conv_opts.enable_autolinking;
+	console.log("(un)checked, now ", conv_opts);
+}
+
+var shitconvert = function(){
+	console.log("converting with options: ", conv_opts);
+	var converter = new Showdown.converter(conv_opts);	
+}*/
+
+var nerdzpreview = function(){
+	document.location.href = "https://www.nerdz.eu/preview.php?message="+document.getElementById("bb").value;
+}
+
+window.onload = function(){
+	//var converter = new Showdown.converter(conv_opts);
+	var source = document.getElementById("md");
+	var target = document.getElementById("bb");
+	//var options = document.body.getElementsByTagName('input');
+
+	source.addEventListener('keyup', function() {
+		target.value = marked(source.value);
+	}, false);
+/*
+	for(var i = 0; i < options.length; ++i)
+		options[i].addEventListener('click', function() {
+			converter = new Showdown.converter(conv_opts);
+			target.value = converter.makeBBCode(source.value);
+		}, false);*/
+
+	//automatically convert the actual text inside "md" textarea at page load.
+	target.value = marked(source.value);
+}
